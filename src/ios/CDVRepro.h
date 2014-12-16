28712655e1b0764ab35c81c199e36a2e3d8dd15e
jollyjoester <jollyjoester@gmail.com>
2014-12-16 11:37:52 +0900

    Add Cordova plugin for iOS

---
+++ src/ios/CDVRepro.h
@@ -0,0 +1,13 @@
+//
+//  CDVRepro.h
+//  HelloWorld
+//
+//  Created by jollyjoester_pro on 2014/12/10.
+//  Copyright (c) 2014 Repro Inc. All rights reserved.
+//
+
+#import <Cordova/CDV.h>
+
+@interface CDVRepro : CDVPlugin
+
+@end
